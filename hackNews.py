1b81844f11a8e4c26e18a91fbc1e2b3219875081
Deep Aggarwal <deep.uiuc@gmail.com>
2015-11-19 23:15:47 -0600

    Updated hackNews.py

--- hackNews.py
+++ hackNews.py
@@ -5,31 +5,79 @@ import sys
 import urllib2
 import webbrowser
 
-#Global variables
+############################
+##### Global variables #####
+############################
+argsDict = {} #or use dict()
+resultCode = {
+    "result_ok": "No error",
+    "result_flag_error": "Error with flag(s)."
+}
 usage = """\
-usage: ./hackNews [-h | -help]
-                  [-p <number of pages to scan>]
-                  [-c <categories to search for>]
-                  [-n <number of pages to open at a time> | <all>]
-                  [-points <points used to display pages with points higher than this>]
+usage: hackNews [-h | -help]
+                [-p <number of pages to scan>]
+                [-c <categories to search for>]
+                [-n <number of pages to open at a time> | <all>]
+                [-points <points used to display pages with points higher than this>]
+
+Syntax for flags:
+    -p:         <Integer greater than 0>
+    -c:         <category1|category2|category3|...>
+    -n:         <Integer greater than 0>
+    -points:    <Integer greater than 0>
+
+Default values of flags:
+    -p:         Default value is 1
+    -c:         None; a value is needed
+    -n:         Default value is 1
+    -points:    Default value is 0
+
+Example usage: hackNews -p 2 -c c++|linux|apache -n 3
+               hackNews -c script -n 2
+               hackNews -c os|guide|database|jquery|rust -points 150
+
+Additional information:
+    1. The -c flag is needed. Other flags are optional.
+    2. Google Chrome web browser is needed.
+    3. urllib2 and webbrowser libraries are needed.
 """
-commandLineArgs = sys.argv
 
-def checkIfValidArgs(args):
-    return False
+#./hackNews [-p <number of pages to scan>] [-c <categories to search for>] [-n <number of pages to open at a time> | <all>] [-points <points used to display pages with points higher than this>]
+#0           1      2                        3               4              5          6                                          7          8
+def parseArguments(arguments):
+    argumentsLen = len(arguments)
 
-def main():
-    if(len(commandLineArgs) < 2):
+    #Check whether number of arguments is odd or not
+    if(argumentsLen % 2 == 0):
+        return resultCode["result_flag_error"]
+
+    numOfIterations = ((argumentsLen-1)/2)
+    """
+    while(numOfIterations > 0):
+        arguments[]
+
+
+        numOfIterations -= 1;
+    """
+
+
+
+
+def main(argc, argv):
+    #Check if the script is ran without any arguments
+    if(argc < 2):
         print usage
         sys.exit()
 
-    if(commandLineArgs[1] == "-h" or commandLineArgs[1] == "-help" or commandLineArgs[1] == "h" or commandLineArgs[1] == "help"):
+    #If help command is passed, print usage and quit
+    if(argv[1] == "-h" or argv[1] == "-help" or argv[1] == "h" or argv[1] == "help"):
         print usage
         sys.exit()
 
-    isValidArgs  = checkIfValidArgs(commandLineArgs)
-    if(isValidArgs == False):
-        print "Error in command line arguments."
+    #Parse the arguments
+    result = parseArguments(argv)
+    if(result == resultCode["result_flag_error"]):
+        print result
         print usage
         sys.exit()
 
@@ -37,7 +85,6 @@ def main():
     print "--------------------------"
     print "\n"
 
-
-
 if __name__ == "__main__":
-    main()
+    commandLineArgs = sys.argv
+    main(len(commandLineArgs), commandLineArgs)
