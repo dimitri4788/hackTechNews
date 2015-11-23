a15437ac07d6a1e05b1a6fb4814019bedfd65a98
Deep Aggarwal <deep.uiuc@gmail.com>
2015-11-23 01:28:49 -0600

    Updated to parseArguments()

--- hackNews.py
+++ hackNews.py
@@ -13,7 +13,11 @@ resultCode = {
     "resultOk": "No error",
     "resultFlagError": "Error with flag(s)."
 }
-hackerNewsURL = "https://news.ycombinator.com/"
+hackerNewsURL = "https://news.ycombinator.com"
+#pathForMorePages = "/news?p=" XXX
+#pathForOverPoints = "/over?points=" XXX
+#https://news.ycombinator.com/over?points=200         XXX
+#https://news.ycombinator.com/over?points=50&p=3      XXX
 usage = """\
 usage: hackNews [-h | -help]
                 [-p <number of pages to scan>]
@@ -43,8 +47,13 @@ Additional information:
     3. urllib2 and webbrowser libraries are needed.
 """
 
-##
 # @brief This function parses the command line arguments and fills the global variable argsDict
+#   NOTE:
+#   Possible flag keys: -p, -c, -n, -points
+#   Possible flag values: <20>, <os,guide,database> etc.
+#   For example: hackNews -p 2 -c c++,linux,apache -n 3
+#                keys: -p, -c, -n
+#                values: 2, c++,linux,apache, 3
 #
 # @param arguments The command line arguments to parse
 #
@@ -67,12 +76,32 @@ def parseArguments(arguments):
     #Iterate over the flagKeyIndex and flagValueIndex and fill argsDict
     expectedFlagKeys = ["-p", "-c", "-n", "-points"]
     for (i,j) in zip(flagKeyIndex,flagValueIndex):
+        #Check whether flag key is in the expectedFlagKeys or not
         if(arguments[i] not in expectedFlagKeys):
             return resultCode["resultFlagError"]
+
+        #Check whether flag value is the correct format and type
+        if(arguments[i] == "-p" or arguments[i] == "-n" or arguments[i] == "-points"):
+            if(arguments[j].isdigit() == False or int(arguments[j]) < 1):
+                return resultCode["resultFlagError"]
+        #elif(arguments[i] == "-c"):
+            #NOTE:Any value can be passed to -c flag key, but if user
+            #inputs some wrong formatted data, then nothing can be done
 
         argsDict[arguments[i]] = arguments[j]
 
     return resultCode["resultOk"]
+
+# @brief Processes the request based on arguments in argsDict and
+#   forms the output
+#
+# @return  It returns the 
+def processRequest():
+    print "Processes Request"
+    if '-points' in argsDict:
+        print "YESSSSSS"
+
+
 
 def main(argc, argv):
     #Check if the script is ran without any arguments
@@ -92,12 +121,14 @@ def main(argc, argv):
         print usage
         sys.exit()
 
-    #Form the 
-
     print "----- Hack Tech News -----"
     print "--------------------------"
     print "\n"
-    print argsDict
+    print argsDict #XXX
+    processRequest()
+    #generate keywords for project XXX
+    #also, make a list suggetion on keywords XXX
+
 
 if __name__ == "__main__":
     commandLineArgs = sys.argv
